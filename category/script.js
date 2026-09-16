db0bf23a809cebc0d387ec30c1496a89af675430
Victoria <vicbru06344@stud.noroff.no>
2026-09-16 16:21:49 +0200

    feat: legg til kategoriside for mens clothing

---
+++ category/script.js
@@ -0,0 +1,38 @@
+async function getMensProducts() {
+  try {
+    const response = await fetch("https://v2.api.noroff.dev/rainy-days");
+    const data = await response.json();
+
+    const mensProducts = data.data.filter((product) => product.gender === "Male");
+
+    const productContainer = document.querySelector("#productlist");
+
+    mensProducts.forEach((product) => {
+      const short = document.createElement("div");
+      short.classList.add("productshort");
+
+      const lenke = document.createElement("a");
+      lenke.href = `../product/index.html?id=${product.id}`;
+
+      const picture = document.createElement("img");
+      picture.src = product.image.url;
+      picture.alt = product.image.alt;
+
+      const title = document.createElement("h3");
+      title.textContent = product.title;
+
+      const price = document.createElement("p");
+      price.textContent = `${product.price} dollar`;
+
+      lenke.appendChild(picture);
+      lenke.appendChild(title);
+      lenke.appendChild(price);
+      short.appendChild(lenke);
+      productContainer.appendChild(short);
+    });
+  } catch (error) {
+    console.error("Something went wrong", error);
+  }
+}
+
+getMensProducts();
